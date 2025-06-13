3672a16b477a441e7707a94fb41f01bd3953fcf6
RAMADAN-MAHDY <104697578+RAMADAN-MAHDY@users.noreply.github.com>
2025-06-13 09:26:07 +0300

    Clean initial commit without large files

---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from sentence_transformers import SentenceTransformer
+import numpy as np
+
+# تحميل الموديل
+# model = SentenceTransformer("local_models/multilingual-e5-small")
+model = SentenceTransformer("intfloat/multilingual-e5-small")
+
+
+app = FastAPI()
+
+# الموديل بتاع الطلب
+class TextInput(BaseModel):
+    text: str
+
+@app.post("/embed")
+def get_embedding(data: TextInput):
+    embedding = model.encode(data.text)
+    return {"embedding": embedding.tolist()}
